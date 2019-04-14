05d803a4904a6f25ac1133add46400363cd068e0
orizitzer365 <orizit365@gmail.com>
2019-04-14 23:16:25 +0300

    Add project files.

---
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,46 @@
+﻿using FlightSimulator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotViewModel:BaseNotify
+    {
+        public string VM_Text { get; set; }
+        private ICommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return  _clearCommand??(_clearCommand =
+                new CommandHandler(() => ClickClear()));
+            }
+        }
+        public void ClickClear()
+        {
+            VM_Text = "";
+            NotifyPropertyChanged("VM_Text");
+        }
+        public string VM_Background { get; set; }
+        private ICommand _okCommand;
+        public ICommand OKCommand
+        {
+            get
+            {
+                return _okCommand ?? (_okCommand =
+                new CommandHandler(() => ClickClear()));
+            }
+        }
+        public void ClickOK()
+        {
+            //send the text to the model
+            VM_Background = "Red";
+            NotifyPropertyChanged("VM_Background");
+        }
+    }
+    
+}
---
+++ FlightSimulator/ViewModels/ManualViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.ViewModels.Windows
+{
+    class ManualViewModel:BaseNotify
+    {
+        private double throttle;
+        public double VM_throttle { get { return Math.Floor( throttle*10)/10; }
+            set {
+                throttle = value;
+                NotifyPropertyChanged("VM_throttle");
+                }
+        }
+        private double rudder;
+        public double VM_rudder {
+            get { return Math.Floor(rudder * 10) / 10; }
+            set
+            {
+                rudder = value;
+                NotifyPropertyChanged("VM_rudder");
+            }
+        }
+        public int VM_aileron { get; set; }
+        public int VM_elevator { get; set; }
+    }
+}
